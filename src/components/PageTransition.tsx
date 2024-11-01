04faf8bb4247b41a4acec478f725022cdbeefda7
Freddy Trae Torres <ftorres0448@gmail.com>
2024-11-01 17:38:16 -0500

    Fixing ESLint issues

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -2,7 +2,7 @@
 
 import {AnimatePresence, motion} from "framer-motion";
 import { usePathname } from "next/navigation";
-const PageTransition = ({children}) => {
+const PageTransition = (children:never) => {
     const pathname = usePathname();
     return (<AnimatePresence>
         <div key={pathname}>
